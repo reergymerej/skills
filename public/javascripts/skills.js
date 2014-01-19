c1167e3f3da40d7cc503935baefdc981a4060afc
Jeremy Greer <jeremygreer@reergymerej.com>
2014-01-18 19:34:32 -0500

    minor ui tweaks

--- public/javascripts/skills.js
+++ public/javascripts/skills.js
@@ -16,14 +16,39 @@ $(function () {
       return dir;
     });
 
+    // function getSkill(tech) {
+    //   var foundSkill;
+    //   $.each(skills, function (i, skill) {
+    //     if (skill.tech === tech) {
+    //       foundSkill = skill;
+    //       return false;
+    //     }
+    //   });
+    //   return foundSkill;
+    // }
+
+    // function showStats(skill) {
+    //   $('.skill').html(skill.skill);
+    // }
+
     function createSkill (i, skill) {
       var $div = $('<div />', {
         html: skill.tech,
         css: {
-          fontSize: (1 + skill.skill / 10) + 'em',
+          // fontSize: (1 + skill.skill / 10) + 'em',
+          fontSize: (10 + skill.skill * 3) + 'px',
           opacity: skill.interest / 10
         },
         class: skill.type
+      });
+
+      $div.hover(
+        function () {
+          var skill = getSkill($(this).html());
+          showStats(skill);
+        },
+        function () {
+
       });
 
       $container.append($div);
